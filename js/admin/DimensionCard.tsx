6ddca6bcef97ed855a87ec8f92117b512faaee2a
Jaiden Mispy <jaiden@mispy.me>
2017-11-09 03:30:36 +0800

    Working out automation standard

--- js/admin/DimensionCard.tsx
+++ js/admin/DimensionCard.tsx
@@ -5,8 +5,16 @@ import DimensionWithData from '../charts/DimensionWithData'
 import ChartEditor from './ChartEditor'
 import { TextField, NumberField, Toggle } from './Forms'
 import { toString } from '../charts/Util'
-import Card, { CardActions, CardContent } from 'material-ui/Card';
-import Grid from 'material-ui/Grid';
+import Card, { CardHeader, CardActions, CardContent } from 'material-ui/Card'
+import Grid from 'material-ui/Grid'
+
+export class OverrideToggle extends React.Component<{ value: boolean, onValue: (v: boolean) => void }> {
+    render() {
+        return <div className={"OverrideToggle" + (this.props.value ? " active" : "")} onClick={() => this.props.onValue(!this.props.value)}>
+            {this.props.value ? <i className="fa fa-child" title="Manual input"/> : <i className="fa fa-cogs" title="Automatic"/>}
+        </div>
+    }
+}
 
 @observer
 export default class DimensionCard extends React.Component<{ dimension: DimensionWithData, editor: ChartEditor, onEdit?: () => void, onRemove?: () => void }> {
@@ -53,33 +61,36 @@ export default class DimensionCard extends React.Component<{ dimension: Dimensio
         const { chart } = editor
 
         return <Card className="DimensionCard">
-            <header>
-                <div>
-                    {this.props.onEdit && <span className="clickable" onClick={this.props.onEdit} style={{ 'margin-right': '10px' }}><i className="fa fa-exchange" /></span>}
-                    {this.props.onRemove && <span className="clickable" onClick={this.props.onRemove} style={{ 'margin-right': '10px' }}><i className="fa fa-times" /></span>}
-                </div>
-                <div>{dimension.variable.name}</div>
-                <div>
-                    {this.hasExpandedOptions && <span className="clickable" onClick={this.onToggleExpand}><i className={"fa fa-chevron-" + (this.isExpanded ? 'up' : 'down')} /></span>}
-                </div>
-            </header>
-            {this.isExpanded && <div>
+            <CardContent>
                 <Grid container spacing={16}>
-                    <Grid item xs={8}><TextField label="Display name" value={dimension.displayName} onValue={this.onDisplayName} /></Grid>
-                    <Grid item xs={4}><Toggle value={dimension.props.displayName !== undefined} onValue={v => dimension.props.displayName = v ? dimension.displayName : undefined}/></Grid>
-                    <Grid item xs={8}><TextField label="Unit of measurement" value={dimension.unit} onValue={this.onUnit} helpText={`Original database unit: ${dimension.variable.unit}`}/></Grid>
-                    <Grid item xs={4}><Toggle value={dimension.props.unit !== undefined} onValue={v => dimension.props.unit = v ? dimension.unit : undefined}/></Grid>
-                    <Grid item xs={8}><TextField label="Short (axis) unit" value={dimension.shortUnit} onValue={this.onShortUnit}/></Grid>
-                    <Grid item xs={4}><Toggle value={dimension.props.shortUnit !== undefined} onValue={v => dimension.props.shortUnit = v ? dimension.shortUnit : undefined}/></Grid>
-                    <Grid item xs={8}><NumberField label="Unit conversion factor" value={dimension.unitConversionFactor} onValue={this.onConversionFactor}/></Grid>
-                    <Grid item xs={4}><Toggle value={dimension.props.conversionFactor !== undefined} onValue={v => dimension.props.conversionFactor = v ? dimension.unitConversionFactor : undefined}/></Grid>
+                    <Grid item xs={12}>
+                        <header>
+                            <div>
+                                {this.props.onEdit && <span className="clickable" onClick={this.props.onEdit} style={{ 'margin-right': '10px' }}><i className="fa fa-exchange" /></span>}
+                                {this.props.onRemove && <span className="clickable" onClick={this.props.onRemove} style={{ 'margin-right': '10px' }}><i className="fa fa-times" /></span>}
+                            </div>
+                            <div>{dimension.variable.name}</div>
+                            <div>
+                                {this.hasExpandedOptions && <span className="clickable" onClick={this.onToggleExpand}><i className={"fa fa-chevron-" + (this.isExpanded ? 'up' : 'down')} /></span>}
+                            </div>
+                        </header>
+                    </Grid>
                 </Grid>
+                {this.isExpanded && <Grid container spacing={16}>
+                        <Grid item xs={8}><TextField label="Display name" value={dimension.displayName} onValue={this.onDisplayName} /></Grid>
+                        <Grid item xs={4}><OverrideToggle value={dimension.props.displayName !== undefined} onValue={v => dimension.props.displayName = v ? dimension.displayName : undefined}/></Grid>
+                        <Grid item xs={8}><TextField label="Unit of measurement" value={dimension.unit} onValue={this.onUnit} helpText={`Original database unit: ${dimension.variable.unit}`}/></Grid>
+                        <Grid item xs={4}><OverrideToggle value={dimension.props.unit !== undefined} onValue={v => dimension.props.unit = v ? dimension.unit : undefined}/></Grid>
+                        <Grid item xs={8}><TextField label="Short (axis) unit" value={dimension.shortUnit} onValue={this.onShortUnit}/></Grid>
+                        <Grid item xs={4}><OverrideToggle value={dimension.props.shortUnit !== undefined} onValue={v => dimension.props.shortUnit = v ? dimension.shortUnit : undefined}/></Grid>
+                        <Grid item xs={8}><NumberField label="Unit conversion factor" value={dimension.unitConversionFactor} onValue={this.onConversionFactor}/></Grid>
+                        <Grid item xs={4}><OverrideToggle value={dimension.props.conversionFactor !== undefined} onValue={v => dimension.props.conversionFactor = v ? dimension.unitConversionFactor : undefined}/></Grid>
 
-                {(chart.isScatter || chart.isDiscreteBar) && <NumberField label="Tolerance" value={dimension.props.tolerance} onValue={this.onTolerance} placeholder={toString(dimension.tolerance)} />}
-                {chart.isLineChart && <Toggle label="Is projection" value={dimension.isProjection} onValue={this.onIsProjection} />}
-                <hr />
-                <Toggle label="Use these settings as defaults for future charts" value={!!dimension.props.saveToVariable} onValue={this.onSaveToVariable} />
-            </div>}
+                    {(chart.isScatter || chart.isDiscreteBar) && <NumberField label="Tolerance" value={dimension.props.tolerance} onValue={this.onTolerance} placeholder={toString(dimension.tolerance)} />}
+                    {chart.isLineChart && <Toggle label="Is projection" value={dimension.isProjection} onValue={this.onIsProjection} />}
+                    <Toggle label="Use these settings as defaults for future charts" value={!!dimension.props.saveToVariable} onValue={this.onSaveToVariable} />
+                </Grid>}
+            </CardContent>
         </Card>
     }
 }
